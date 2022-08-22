6fce8e0302488636c82b4b1d67eac591b38bfc68
elfsvet <stepanmatysik@gmail.com>
2022-08-21 23:27:45 -0400

    directory page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -29,12 +28,9 @@ const App = () => {
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
-
   return (
     <div className='categories-container'>
-      {categories.map((category) => (
-        <CategoryItem category={category} key={category.id} />
-      ))}
+      <Directory categories={categories} />
     </div>
   );
 };
