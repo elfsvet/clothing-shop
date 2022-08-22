edcd8728e6a4923c74efaadd73024e811f2b7f4f
elfsvet <stepanmatysik@gmail.com>
2022-08-21 22:42:54 -0400

    added the containers

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ const App = () => {
   return (
     <div className='categories-container'>
       <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
+        {/* <imp/> */}
+        <div className='category-container'>
           <h2>Hats</h2>
           <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
+        {/* <imp/> */}
+        <div className='category-container'>
           <h2>Jackets</h2>
           <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
+        {/* <imp/> */}
+        <div className='category-container'>
           <h2>Sneakers</h2>
           <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
+        {/* <imp/> */}
+        <div className='category-container'>
           <h2>Womens</h2>
           <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
+        {/* <imp/> */}
+        <div className='category-container'>
           <h2>Mens</h2>
           <p>Shop Now</p>
         </div>
