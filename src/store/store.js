cd7830ef8722eeb0dcb6ee8f80f55dec7faf4ecd
elfsvet <stepanmatysik@gmail.com>
2022-09-20 10:08:31 -0400

    added logger and installed reselect

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,23 @@ import { rootReducer } from './root-reducer';
 // root reducer - 1 big reducer
 
 // to create a store we would need only root reducer
-const middleWares = [logger];
+
+
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type){
+        return next(action);
+    }
+
+    console.log('type: ' , action.type);
+    console.log('payload: ' , action.payload);
+    console.log('currentState: ', store.getState())
+
+    next(action);
+
+    console.log('next state: ', store.getState())
+}
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 // but to be usefull we also need to pass the logger
